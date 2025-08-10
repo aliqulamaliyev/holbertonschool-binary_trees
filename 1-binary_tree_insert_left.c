2a263b62d500e484127e923c5b4a5b7106a48596
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-08-10 17:49:26 +0000

    completes all c tasks

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,11 +23,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = parent->left;
 	new_node->right = NULL;
-	
-	
+
 	if (parent->left != NULL)
 		parent->left->parent = new_node;
 	parent->left = new_node;
-	
+
 	return (new_node);
 }
